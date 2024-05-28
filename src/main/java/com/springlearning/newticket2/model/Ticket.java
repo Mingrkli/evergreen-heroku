680081e7962a7c117a33f7fd5b400ede93942ea5
Ming Li <Mingrkli@gmail.com>
2024-05-28 00:44:54 -0700

    New changes

--- src/main/java/com/springlearning/newticket2/model/Ticket.java
+++ src/main/java/com/springlearning/newticket2/model/Ticket.java
@@ -1,5 +1,6 @@
 package com.springlearning.newticket2.model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
@@ -38,13 +39,6 @@ public class Ticket {
     private String name;
 
     private String created;
-    @NotNull(message = "Please Describe How We Can Help You")
-    @NotBlank(message = "Please Describe How We Can Help You")
-//    @Size(min=50, max=700, message =
-//            "Please provide a detailed description of the issue " +
-//                    "we can assist you with. Minimum description must be " +
-//                    "at least 50 characters and should not exceed 1000 " +
-//                    "characters...")
     private String lastMessage; // change to date
     private String status;
     
